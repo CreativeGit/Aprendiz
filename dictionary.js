94ad4e65360cff42b449945c443aaa5f3a2b6eb2
CreativeGit <79688041+CreativeGit@users.noreply.github.com>
2021-10-10 19:47:33 +0530

    Add files via upload

--- dictionary.js
+++ dictionary.js
@@ -4,28 +4,28 @@ const dictionary = {
 		"french": "est",
 		"german": "ist",
 		"spanish": "es",
-		"r": 80
+		"r": 20
 	},
 	"as": {
 		"dutch": "als",
 		"french": "comme",
 		"german": "wie",
 		"spanish": "como",
-		"r": 80
+		"r": 20
 	},
 	"that": {
 		"dutch": "dat",
 		"french": "cette",
 		"german": "das",
 		"spanish": "ese",
-		"r": 80
+		"r": 20
 	},
 	"the": {
 		"dutch": "de",
 		"french": "les",
 		"german": "das",
 		"spanish": "los",
-		"r": 80
+		"r": 20
 	},
 	"how": {
 		"dutch": "hoe",
@@ -67,7 +67,7 @@ const dictionary = {
 		"french": "pour",
 		"german": "zum",
 		"spanish": "por",
-		"r": 80
+		"r": 20
 	},
 	"soon": {
 		"dutch": "spoedig",
@@ -109,7 +109,7 @@ const dictionary = {
 		"french": "chaud",
 		"german": "heiße",
 		"spanish": "caliente",
-		"r": 80
+		"r": 20
 	},
 	"cold": {
 		"dutch": "koud",
@@ -1026,13 +1026,6 @@ const dictionary = {
 		"french": "sixième",
 		"german": "sechste",
 		"spanish": "sexto",
-		"r": 80
-	},
-	"seventh": {
-		"dutch": "zevende",
-		"french": "septième",
-		"german": "siebte",
-		"spanish": "séptimo",
 		"r": 80
 	},
 	"eight": {
